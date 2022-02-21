e84ae2d3debdad97e926ea8d8f4e63c1c5db5b7e
cliff <cliff.wong@ehlacademy.org>
2022-02-21 16:55:58 +0800

    tailwind css

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Popup from '$lib/app-shelf/popup.svelte';
 	import Modal from '$lib/app-shelf/modal.svelte';
-	import '../app.css';
+	import '../styles/tailwind-output.css';
 </script>
 
 <Modal>
