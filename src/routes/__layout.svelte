b7c165e70828d05c9d42104719d9d8cf4f6b1f19
cliff <cliff.wong@ehlacademy.org>
2022-02-22 14:20:38 +0800

    move popup inside writing compoennt

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,10 @@
 <script>
-	import Popup from '$lib/app-shelf/popup.svelte';
 	import Modal from '$lib/app-shelf/modal.svelte';
 	import '../styles/tailwind-output.css';
 </script>
 
 <Modal>
-<Popup>
 <main>
 	<slot />
 </main>
-</Popup>
 </Modal>
