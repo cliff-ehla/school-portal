50e1c898af0d092844dbb16543027f9e221327c4
cliff <cliff.wong@ehlacademy.org>
2022-03-23 17:25:06 +0800

    loading bar

---
+++ src/lib/app-shelf/indeterminate-loading-bar.svelte
@@ -0,0 +1,20 @@
+<div class="h-1 relative rounded-full">
+	<div class="absolute left-0 w-full h-full rounded-full bg-gradient-to-r from-yellow-500 to-red-500 animation"></div>
+</div>
+
+<style>
+	.animation {
+			animation: load 1s linear infinite;
+	}
+
+	@keyframes load {
+			from {
+					left: -100%;
+					width: 100%;
+			}
+			to {
+					left: 100%;
+          width: 10%;
+			}
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,19 @@
 <script>
 	import Modal from '$lib/app-shelf/modal.svelte';
 	import TopBar from '$lib/app-shelf/top-bar.svelte';
+	import LoadingBar from '$lib/app-shelf/indeterminate-loading-bar.svelte'
+	import {navigating} from "$app/stores";
 	import '../styles/tailwind-output.css';
 	import {page} from "$app/stores";
 </script>
 
 <Modal>
 <main>
+	{#if $navigating}
+		<div class="fixed inset-x-0 top-0 z-50">
+			<LoadingBar/>
+		</div>
+	{/if}
 	{#if $page.url.pathname !== '/login' && $page.url.pathname !== '/logout'}
 	<TopBar/>
 	{/if}
