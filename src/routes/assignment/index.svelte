8957638a52fc252b29d25bb23e746eca39759821
cliff <cliff.wong@ehlacademy.org>
2022-03-24 17:22:34 +0800

    calendar popup draft

--- src/routes/assignment/index.svelte
+++ src/routes/assignment/index.svelte
@@ -1,1 +1,11 @@
-assignement
+<div class="p-4">
+	<div class="p-4 shadow max-w-[400px]">
+		<p>Speaking exercise</p>
+		<p class="font-bold">6A</p>
+		<p>Feb 25</p>
+		<p>Speaking of wisdom</p>
+		<p>25 submitted</p>
+		<p>3 late</p>
+		<p>4 below average</p>
+	</div>
+</div>
